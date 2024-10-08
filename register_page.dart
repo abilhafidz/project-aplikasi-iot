83f5e171443388c84de6894567796a75e7e90c2e
abilhafidz <158029229+abilhafidz@users.noreply.github.com>
2024-10-08 21:34:04 +0700

    register_page.dart

---
+++ register_page.dart
@@ -0,0 +1,202 @@
+import 'package:flutter/material.dart';
+
+class RegisterPage extends StatefulWidget {
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  bool _obscureText = true; 
+  bool _isHovered = false; 
+
+ 
+  bool get _isInputFilled => _usernameController.text.isNotEmpty && _passwordController.text.isNotEmpty;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF58403B), 
+      body: Stack(
+        children: [
+          
+          Positioned(
+            top: 0,
+            right: 0,
+            child: Container(
+              width: 187,
+              height: 47,
+              decoration: BoxDecoration(
+                color: Color(0xFFE5C6B6), 
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(50), 
+                ),
+              ),
+            ),
+          ),
+          
+          Positioned(
+            bottom: 0,
+            left: 0,
+            child: Container(
+              width: 187,
+              height: 47,
+              decoration: BoxDecoration(
+                color: Color(0xFFE5C6B6), 
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(50), 
+                ),
+              ),
+            ),
+          ),
+         
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 24.0), 
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  // Logo Smart Home
+                  Image.asset(
+                    'assets/images/logo_rumah.png', 
+                    height: 150, 
+                  ),
+                  SizedBox(height: 20), 
+                  Text(
+                    'SMART HOME',
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 24,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  ),
+                  SizedBox(height: 40), 
+                  
+                  Card(
+                    color: Color(0xFFFAE7D7), 
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0), 
+                    ),
+                    elevation: 4, 
+                    child: Padding(
+                      padding: const EdgeInsets.all(24.0), 
+                      child: Column(
+                        mainAxisSize: MainAxisSize.min,
+                        children: [
+                        
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black54), 
+                              borderRadius: BorderRadius.circular(8),
+                              color: Colors.white,
+                            ),
+                            child: TextField(
+                              controller: _usernameController,
+                              style: TextStyle(color: Colors.black),
+                              decoration: InputDecoration(
+                                hintText: 'username',
+                                hintStyle: TextStyle(color: Colors.black54), 
+                                border: InputBorder.none, 
+                                contentPadding: EdgeInsets.all(12),
+                              ),
+                              onChanged: (value) {
+                                setState(() {}); 
+                              },
+                            ),
+                          ),
+                          SizedBox(height: 20), 
+                        
+                          Container(
+                            decoration: BoxDecoration(
+                              border: Border.all(color: Colors.black54), 
+                              borderRadius: BorderRadius.circular(8),
+                              color: Colors.white,
+                            ),
+                            child: TextField(
+                              controller: _passwordController,
+                              obscureText: _obscureText,
+                              style: TextStyle(color: Colors.black),
+                              decoration: InputDecoration(
+                                hintText: 'password',
+                                hintStyle: TextStyle(color: Colors.black54),
+                                border: InputBorder.none, 
+                                contentPadding: EdgeInsets.all(12), 
+                                suffixIcon: IconButton(
+                                  icon: Icon(
+                                    _obscureText ? Icons.visibility : Icons.visibility_off,
+                                    color: Colors.black54,
+                                  ),
+                                  onPressed: () {
+                                    setState(() {
+                                      _obscureText = !_obscureText; 
+                                    });
+                                  },
+                                ),
+                              ),
+                              onChanged: (value) {
+                                setState(() {}); 
+                              },
+                            ),
+                          ),
+                          SizedBox(height: 30), 
+                          
+                          GestureDetector(
+                            onTap: _isInputFilled ? () {
+                              
+                            } : null, 
+                            child: MouseRegion(
+                              onEnter: (_) {
+                                if (_isInputFilled) {
+                                  setState(() => _isHovered = true);
+                                }
+                              },
+                              onExit: (_) {
+                                setState(() => _isHovered = false);
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  border: Border.all(color: Colors.black54), 
+                                  borderRadius: BorderRadius.circular(8),
+                                  color: _isHovered && _isInputFilled ? Color(0xFF4E3B31) : Color(0xFFFAE7D7), 
+                                ),
+                                child: Padding(
+                                  padding: EdgeInsets.symmetric(vertical: 12, horizontal: 40),
+                                  child: Text(
+                                    'SIGN UP',
+                                    style: TextStyle(
+                                      color: _isHovered && _isInputFilled ? Colors.white : Colors.black,
+                                    ),
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                          SizedBox(height: 20), 
+                          
+                          GestureDetector(
+                            onTap: () {
+                              Navigator.pop(context); 
+                            },
+                            child: Text(
+                              'kembali',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 14,
+                                decoration: TextDecoration.underline,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
