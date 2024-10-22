0cd10ef62690825cad2cbaa962c18860d0df7038
raynard05 <150323912+raynard05@users.noreply.github.com>
2024-10-22 16:55:00 +0700

    Create main.dart
    file main.dart dengan inisialisasi firebase, changenotifierprovider untuk mqttservice dan switchstate (penyimpanan value) , dan rute navigasi

---
+++ main.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'login_page.dart'; 
+import 'dashboard.dart'; 
+import 'switch_state.dart'; 
+import 'mqtt_service.dart'; 
+import 'package:firebase_auth/firebase_auth.dart'; 
+import 'package:firebase_core/firebase_core.dart'; 
+import 'firebase_options.dart'; 
+import 'splash_screen.dart'; 
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized(); 
+
+  
+  try {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform, 
+    );
+  } catch (e) {
+    print("Failed to initialize Firebase: $e"); 
+  }
+
+  
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => SwitchState(MqttService()),
+      child: SmartHomeApp(),
+    ),
+  );
+}
+
+class SmartHomeApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: SplashScreen(), 
+      debugShowCheckedModeBanner: false, 
+      routes: {
+        '/login': (context) => LoginPage(),
+
+      },
+    );
+  }
+}
+
